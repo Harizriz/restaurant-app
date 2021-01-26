05a95d39f14b8dda76c6ff49f68587f4ed275ff8
hariz <hariz1807@gmail.com>
2021-01-27 03:02:16 +0800

    changed stateless component to class component

--- assets/screens/LoginScreen/LoginPhoneNumberScreen.js
+++ assets/screens/LoginScreen/LoginPhoneNumberScreen.js
@@ -1,54 +1,60 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { SafeAreaView, View, StyleSheet, Text } from "react-native";
 import Button from "react-native-button";
 
 import AlertButton from "../../components/AlertButton";
 import PromptTextInput from "../../components/InputText";
 
-const LoginPhoneNumberScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.headingContainer}>
-        <Text style={styles.headingText}>Sign In</Text>
-      </View>
-      <View style={styles.promptContainer}>
-        <PromptTextInput placeholder="Phone Number" />
-      </View>
-      <View style={styles.submitContainer}>
-        <AlertButton
-          buttonText="Send Code"
-          text="A code has been sent to your message!"
-        />
-      </View>
-      <View style={styles.or}>
-        <Text>OR</Text>
-      </View>
-      <View style={styles.bottomContainer}>
-        <Button
-          style={{
-            padding: 16,
-            width: 250,
-            borderRadius: 24,
-            alignItems: "center",
-            backgroundColor: "#3b5998",
-            color: "white",
-            overflow: "hidden",
-          }}
-          onPress={() => navigation.navigate("")}
-        >
-          Login with Facebook
-        </Button>
-        <Text
-          style={styles.loginLink}
-          onPress={() => navigation.navigate("LoginScreen")}
-        >
-          Sign in with Email
-        </Text>
-      </View>
-    </View>
-  );
-};
-
+class LoginPhoneNumberScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <SafeAreaView style={styles.container}>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headingText}>Sign In</Text>
+        </View>
+        <View style={styles.promptContainer}>
+          <PromptTextInput placeholder="Phone Number" />
+        </View>
+        <View style={styles.submitContainer}>
+          <AlertButton
+            buttonText="Send Code"
+            text="A code has been sent to your message!"
+          />
+        </View>
+        <View style={styles.or}>
+          <Text>OR</Text>
+        </View>
+        <View style={styles.bottomContainer}>
+          <Button
+            style={{
+              padding: 16,
+              width: 250,
+              borderRadius: 24,
+              alignItems: "center",
+              backgroundColor: "#3b5998",
+              color: "white",
+              overflow: "hidden",
+            }}
+            onPress={() => this.props.navigation.navigate("")}
+          >
+            Login with Facebook
+          </Button>
+          <Text
+            style={styles.loginLink}
+            onPress={() => this.props.navigation.navigate("LoginScreen")}
+          >
+            Sign in with Email
+          </Text>
+        </View>
+      </SafeAreaView>
+     );
+  }
+}
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
