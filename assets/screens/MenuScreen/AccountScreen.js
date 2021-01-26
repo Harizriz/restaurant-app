3020fb38a8ed348073b513337e9ed493dccf3881
hariz <hariz1807@gmail.com>
2021-01-27 03:11:56 +0800

    changed stateless component to class component

--- assets/screens/MenuScreen/AccountScreen.js
+++ assets/screens/MenuScreen/AccountScreen.js
@@ -1,58 +1,65 @@
-import React from "react";
-import { StyleSheet, View, Text, Alert } from "react-native";
+import React, { Component } from 'react';
+import { SafeAreaView, StyleSheet, View, Text, Alert } from "react-native";
 import InputText from "../../components/InputText";
 import Button from "react-native-button";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-const AccountScreen = ({ navigation }) => {
-  const createTwoButtonAlert = () => Alert.alert("Logging Out", "Are you sure you want to log out?", [
-    { text: "Cancel", onPress: () => console.log("cancelled!") },
-    { text: "Log out", onPress: () => console.log("logout!") },
-  ]);
-  return (
-    <View style={styles.container}>
-      <View style={styles.headingContainer}>
-        <Text style={styles.headingText}>My Account</Text>
-      </View>
-      <View style={styles.profileContainer}>
-        <InputText placeholder="Full Name" />
-        <InputText placeholder="Email" />
-        <InputText placeholder="Phone Number" />
-        <InputText placeholder="Date of Birth" />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          style={{
-            paddingRight: 130,
-            paddingBottom: 10,
-            width: "100%",
-            color: "black",
-            // borderBottomWidth: 1,
-          }}
-          onPress={() => navigation.navigate("ChangePasswordScreen")}
-        >
-          Change Password
-        </Button>
-        <Icon name="chevron-right" color="black" size={26} onPress={() => navigation.navigate("ChangePasswordScreen")}/>
-      </View>
-      <View style={styles.button2Container}>
-        <Button
-          style={{
-            paddingRight: 210,
-            paddingBottom: 10,
-            width: "100%",
-            color: "black",
-            // borderBottomWidth: 1,
-          }}
-          onPress={createTwoButtonAlert}
-        >
-          Log Out
-        </Button>
-        <Icon name="chevron-right" color="black" size={26} onPress={createTwoButtonAlert}/>
-      </View>
-    </View>
-  );
-};
+const createTwoButtonAlert = () => Alert.alert("Logging Out", "Are you sure you want to log out?", [
+  { text: "Cancel", onPress: () => console.log("cancelled!") },
+  { text: "Log out", onPress: () => console.log("logout!") },
+]);
+
+class AccountScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headingText}>My Account</Text>
+        </View>
+        <View style={styles.profileContainer}>
+          <InputText placeholder="Full Name" />
+          <InputText placeholder="Email" />
+          <InputText placeholder="Phone Number" />
+          <InputText placeholder="Date of Birth" />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            style={{
+              paddingRight: 130,
+              paddingBottom: 10,
+              width: "100%",
+              color: "black",
+              // borderBottomWidth: 1,
+            }}
+            onPress={() => this.props.navigation.navigate("ChangePasswordScreen")}
+          >
+            Change Password
+          </Button>
+          <Icon name="chevron-right" color="black" size={26} onPress={() => this.props.navigation.navigate("ChangePasswordScreen")}/>
+        </View>
+        <View style={styles.button2Container}>
+          <Button
+            style={{
+              paddingRight: 210,
+              paddingBottom: 10,
+              width: "100%",
+              color: "black",
+              // borderBottomWidth: 1,
+            }}
+            onPress={createTwoButtonAlert}
+          >
+            Log Out
+          </Button>
+          <Icon name="chevron-right" color="black" size={26} onPress={createTwoButtonAlert}/>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
