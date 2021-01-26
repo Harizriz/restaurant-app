d0afbfa8142dc943d03672cf83c9e57fc775c328
hariz <hariz1807@gmail.com>
2021-01-27 02:55:58 +0800

    changed stateless component to class component

--- assets/screens/SignUpScreen/SignUpPhoneNumberScreen.js
+++ assets/screens/SignUpScreen/SignUpPhoneNumberScreen.js
@@ -1,42 +1,45 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
+import { SafeAreaView, View, StyleSheet, Text, Image } from "react-native";
 
 import PromptTextInput from "../../components/InputText";
 import AlertButton from "../../components/AlertButton";
 
-const SignUpPhoneNumberScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.headingContainer}>
-        <Text style={styles.headingText}>Create new account</Text>
-      </View>
-      {/* <View style={styles.pictureContainer}></View> */}
-      <View style={styles.promptContainer}>
-        <PromptTextInput placeholder="First Name" />
-        <PromptTextInput placeholder="Last Name" />
-        <PromptTextInput placeholder="Phone Number" />
-      </View>
-      <View style={styles.submitContainer}>
-        <AlertButton
-          buttonText="Send Code"
-          text="A code has been sent to your message!"
-        />
-      </View>
-      <View style={styles.or}>
-        <Text>OR</Text>
-      </View>
-      <View style={styles.bottomContainer}>
-        <Text
-          style={styles.loginLink}
-          onPress={() => navigation.navigate("SignUpScreen")}
-        >
-          Sign up with Email
-        </Text>
-      </View>
-    </View>
-  );
-};
-
+class SignUpPhoneNumberScreen extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <SafeAreaView style={styles.container}>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headingText}>Create new account</Text>
+        </View>
+        {/* <View style={styles.pictureContainer}></View> */}
+        <View style={styles.promptContainer}>
+          <PromptTextInput placeholder="First Name" />
+          <PromptTextInput placeholder="Last Name" />
+          <PromptTextInput placeholder="Phone Number" />
+        </View>
+        <View style={styles.submitContainer}>
+          <AlertButton
+            buttonText="Send Code"
+            text="A code has been sent to your message!"
+          />
+        </View>
+        <View style={styles.or}>
+          <Text>OR</Text>
+        </View>
+        <View style={styles.bottomContainer}>
+          <Text
+            style={styles.loginLink}
+            onPress={() => this.props.navigation.navigate("SignUpScreen")}
+          >
+            Sign up with Email
+          </Text>
+        </View>
+      </SafeAreaView>
+     );
+  }
+}
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
