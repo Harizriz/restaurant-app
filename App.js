0861c2dd9e9c7b3cc0e53f8324bb71df20cbbb19
hariz <hariz1807@gmail.com>
2021-01-28 01:47:06 +0800

    removed backend properties from frontend

--- App.js
+++ App.js
@@ -1,28 +1,7 @@
-import React, {useEffect} from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import keys from "./constants/keys";
-import Parse from "parse/react-native";
+import React from "react";
 
 import AppNavigation from "./assets/screens/AppNavigation/AppNavigation";
 
-Parse.setAsyncStorage(AsyncStorage);
-Parse.initialize(keys.applicationId, keys.javascriptKey);
-Parse.serverURL = keys.serverURL;
-
 export default function App() {
-
-  useEffect(() => {
-    const createInstallation = async () => {
-      const Installation = Parse.Object.extend(Parse.Installation);
-      const installation = new Installation();
-  
-      installation.set("deviceType", Platform.OS);
-  
-      await installation.save();
-    }
-
-    createInstallation();
-  }, []);
-
   return <AppNavigation />;
 }
