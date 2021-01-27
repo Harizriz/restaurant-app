ebbc6037f9976b9121c552c127a7a1729964b8ef
hariz <hariz1807@gmail.com>
2021-01-27 18:17:03 +0800

    connect App to Back4App database

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, {useEffect} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import keys from "./constants/keys";
+import Parse from "parse/react-native";
 
 import AppNavigation from "./assets/screens/AppNavigation/AppNavigation";
 
+Parse.setAsyncStorage(AsyncStorage);
+Parse.initialize(keys.applicationId, keys.javascriptKey);
+Parse.serverURL = keys.serverURL;
+
 export default function App() {
+
+  useEffect(() => {
+    const createInstallation = async () => {
+      const Installation = Parse.Object.extend(Parse.Installation);
+      const installation = new Installation();
+  
+      installation.set("deviceType", Platform.OS);
+  
+      await installation.save();
+    }
+
+    createInstallation();
+  }, []);
+
   return <AppNavigation />;
 }
---
+++ constants/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    applicationId:  'mYDYPTzeNxlL6ADEEHqXDWHvQ9wgkLYz1TXde1Rn',
+    javascriptKey:  'tSaYr2DyEfCrVA2SPsldpGIFAyayQC2iLAlcem27',
+    serverURL:  'https://parseapi.back4app.com/'
+}
