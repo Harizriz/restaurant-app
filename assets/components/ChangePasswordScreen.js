e92a498d51754160b515d7b14cc2ea133e433673
hariz <hariz1807@gmail.com>
2021-01-31 23:13:10 +0800

    changed UI

--- assets/components/ChangePasswordScreen.js
+++ assets/components/ChangePasswordScreen.js
@@ -1,62 +1,131 @@
-import React from "react";
-import { StyleSheet, View, Text, Alert } from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Alert, SafeAreaView, TextInput } from "react-native";
 import Button from "react-native-button";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-import PromptTextInput from "../components/InputText";
-
-
-const ChangePasswordScreen = ({ navigation }) => {
-  const createAlert = () => Alert.alert("Password changed successfully!");
-  return (
-    <View style={styles.container}>
-      <View style={styles.headingContainer}>
-        <Text style={styles.headingText}>Change Password</Text>
-      </View>
-      <View style={styles.promptContainer}>
-        <Text style={styles.text}>Current password</Text>
-        <PromptTextInput placeholder="" />
-      </View>
-      <View style={styles.promptContainer}>
-        <Text style={styles.text}>New password</Text>
-        <PromptTextInput placeholder="" />
-      </View>
-      <View style={styles.submitContainer}>
-      <Button
-        style={{
-            fontSize: 15,
-            padding: 16,
-            width: 100,
-            borderRadius: 5,
-            alignItems: "center",
-            backgroundColor: "grey",
-            color: "white",
-            overflow: "hidden",
-            top: 20,
-            left: 30
-        }}
-        onPress={() => navigation.goBack()}>
-        Cancel
-      </Button>
-      <Button
-        style={{
-            fontSize: 15,
-            padding: 16,
-            width: 100,
-            borderRadius: 5,
-            alignItems: "center",
-            backgroundColor: "grey",
-            color: "white",
-            overflow: "hidden",
-            top: 20,
-            left: 50
-        }}
-        onPress={createAlert}>
-        Change
-      </Button>
-      </View>
-    </View>
-  );
-};
+class ChangePasswordScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      CurrentPasswordValueHolder: '',
+      NewPasswordPasswordValueHolder: '',
+      ConfirmPasswordValueHolder: '',
+      icon: "eye-off",
+      password: true
+     };
+  }
+  changeIcon() {
+    this.setState(prevState => ({
+        icon: prevState.icon === 'eye' ? 'eye-off' : 'eye',
+        password: !prevState.password
+    }));
+  }
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headingText}>Change Password</Text>
+        </View>
+        <View style={styles.promptContainer}>
+          <Text style={styles.text}>Current password</Text>
+          <View style={styles.textInputContainer}>
+            <TextInput
+              placeholderTextColor="gray"
+              secureTextEntry={this.state.password}
+              style={{
+                height: 50,
+                borderColor: "gray",
+                borderWidth: 2,
+                borderRadius: 10,
+                width: "75%",
+                paddingLeft: 20,
+                marginTop: 20,
+                left: 8
+              }}
+              onChangeText={CurrentPasswordValueHolder => this.setState({CurrentPasswordValueHolder})}
+            />   
+            <Icon name={this.state.icon} size={17} style={styles.icon} onPress={() => this.changeIcon()} />
+          </View>
+        </View>
+        <View style={styles.promptContainer}>
+          <Text style={styles.text}>New password</Text>
+          <View style={styles.textInputContainer}>
+            <TextInput
+              placeholderTextColor="gray"
+              secureTextEntry={this.state.password}
+              style={{
+                height: 50,
+                borderColor: "gray",
+                borderWidth: 2,
+                borderRadius: 10,
+                width: "75%",
+                paddingLeft: 20,
+                marginTop: 20,
+                left: 8
+              }}
+              onChangeText={NewPasswordPasswordValueHolder => this.setState({NewPasswordPasswordValueHolder})}
+            />   
+            <Icon name={this.state.icon} size={17} style={styles.icon} onPress={() => this.changeIcon()} />   
+          </View>
+        </View>
+        <View style={styles.promptContainer}>
+          <Text style={styles.text}>Confirm New password</Text>
+          <View style={styles.textInputContainer}>
+            <TextInput
+              placeholderTextColor="gray"
+              secureTextEntry={this.state.password}
+              style={{
+                height: 50,
+                borderColor: "gray",
+                borderWidth: 2,
+                borderRadius: 10,
+                width: "75%",
+                paddingLeft: 20,
+                marginTop: 20,
+                left: 8
+              }}
+              onChangeText={ConfirmPasswordValueHolder => this.setState({ConfirmPasswordValueHolder})}
+            />   
+            <Icon name={this.state.icon} size={17} style={styles.icon} onPress={() => this.changeIcon()} />   
+          </View>
+        </View>
+        <View style={styles.submitContainer}>
+        <Button
+          style={{
+              fontSize: 15,
+              padding: 16,
+              width: 200,
+              borderRadius: 5,
+              alignItems: "center",
+              backgroundColor: "#007aff",
+              color: "white",
+              overflow: "hidden",
+              top: 30,
+          }}
+          onPress={() => Alert.alert("Password changed successfully!")}>
+          Update password
+        </Button>
+        {/* <Button
+          style={{
+              fontSize: 15,
+              padding: 16,
+              width: 100,
+              borderRadius: 5,
+              alignItems: "center",
+              backgroundColor: "grey",
+              color: "white",
+              overflow: "hidden",
+              top: 30,
+              left: 50
+          }}
+          onPress={() => this.props.navigation.goBack()}>
+          Cancel
+        </Button> */}
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -79,18 +148,32 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     // alignContent: "center",
     // backgroundColor: "green",
-    marginHorizontal: 50,
+    // marginHorizontal: 50,
+  },
+  textInputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: "yellow"
   },
   headingText: {
-    fontSize: 30,
+    fontSize: 35,
     color: "purple",
-    left: 50,
     top: 50,
+    alignSelf: "center"
   },
   text: {
-      fontSize: 15,
-      left: 50,
-      top: 20
+    fontSize: 20,
+    left: 50,
+    top: 20,
+    fontWeight: "bold",
+    color: "purple",
+  },
+  icon: {
+    right: 30,
+    top: 10,
+    // backgroundColor: "yellow",
   }
 });
 
