b1e53c36195bcd1f89a375a16fea7df47d3a7534
hariz <hariz1807@gmail.com>
2021-02-21 08:16:07 +0800

    added checkout alert

--- assets/screens/RestaurantStaff/CheckoutTableScreen.js
+++ assets/screens/RestaurantStaff/CheckoutTableScreen.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, View, Text, FlatList, Dimensions } from 'react-native';
-import DATA from "../../components/DummyData";
-
-const Item = ({ title }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
-);
-
-const renderItem = ({ item }) => (
-    <Item title={item.id} />
-);
+import { SafeAreaView, StyleSheet, View, Text, FlatList, Dimensions, Alert } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Icon from "react-native-vector-icons/MaterialCommunityIcons"
 
 class CheckoutTableScreen extends Component {
     constructor(props) {
@@ -19,16 +10,49 @@ class CheckoutTableScreen extends Component {
 
         };
     }
+    componentDidMount = async () => {
+        fetch(`http://172.20.10.5:5000/api/tables`)
+        .then(response => response.json())
+        .then(responseJson => {
+            this.setState({
+            dataSource: responseJson
+            });
+        })
+        // auto-refresh the screen
+        this.props.navigation.addListener('focus', () => {
+            fetch(`http://172.20.10.5:5000/api/tables`)
+            .then(response => response.json())
+            .then(responseJson => {
+                this.setState({
+                dataSource: responseJson
+                });
+            })
+        });
+    }
     render() {
+        const Item = ({ qrCodeValue, paxValue }) => (
+            <View style={styles.item}>
+              <Text style={styles.title}>{qrCodeValue}</Text>
+              <Icon name="chair-school" color="gray" size={26}/>
+              <Text style={styles.content}>pax: {paxValue}</Text>
+            </View>
+        );
+        
+        const renderItem = ({ item }) => (
+            <TouchableOpacity onPress={() => 
+                Alert.alert("Checkout Table", "Are you sure you want to checkout the table?", [
+                    { text: "Cancel", onPress: () => console.log("cancelled!") },
+                    { text: "Confirm", onPress: () => console.log("checkout!") },
+                ])}>
+                <Item qrCodeValue={item.qrCodeValue} paxValue={item.paxValue}/>
+            </TouchableOpacity>
+        );
         return (
             <SafeAreaView style={styles.container}>
-                <View style={styles.headingContainer}>
-                    <Text style={styles.headingText}>Checkout Table Screen</Text>
-                </View>
                 <FlatList
-                    data={DATA}
+                    data={this.state.dataSource}
                     renderItem={renderItem}
-                    keyExtractor={(item) => item.id}
+                    keyExtractor={(item) => item.objectId}
                     numColumns={3}
                 />
             </SafeAreaView>
@@ -55,10 +79,10 @@ const styles = StyleSheet.create({
     item: {
         backgroundColor: '#f9c2ff',
         // padding: 20,
-        marginVertical: 8,
+        marginVertical: 7,
         marginHorizontal: 7,
         width: Dimensions.get('window').width * 0.3,
-        height: 100,
+        height: 107,
         borderWidth: 1,
         borderColor: "black",
         alignItems: 'center',
@@ -66,7 +90,18 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 32,
+        // backgroundColor: "yellow"
     },
+    content: {
+        top: 5,
+        fontSize: 24,
+        // backgroundColor: "lightblue"
+    },
+    text: {
+        fontSize: 16,
+        alignSelf: "center",
+        color: "white"
+    }
 });
 
 export default CheckoutTableScreen;
