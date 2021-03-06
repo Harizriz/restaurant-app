88858bfa7114a21fab1ee6c78592ded4eadef140
hariz <hariz1807@gmail.com>
2021-03-07 00:24:47 +0800

    output backend in main menu

--- assets/screens/MenuScreen/MainMenuScreen.js
+++ assets/screens/MenuScreen/MainMenuScreen.js
@@ -1,69 +1,85 @@
-import React from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  SafeAreaView,
-  FlatList,
-  StatusBar,
-} from "react-native";
-import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
-import SafeAreaAndroid from "../../components/SafeViewAndroid";
-
+import React, { Component } from 'react';
+import { SafeAreaView, StyleSheet, View, FlatList, StatusBar, Text } from 'react-native';
 import SearchInput from "../../components/SearchInput";
-import DATA from "../../components/DummyData";
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const Item = ({ title }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
-
-const MainMenuScreen = ({ navigation }) => {
-  const renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => navigation.navigate(item.page)}>
-      <Item title={item.title} />
-    </TouchableOpacity>
-  );
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.headingContainer}>
-        <View style={styles.searchContainer}>
-          <SearchInput placeholder="What are you craving for?" />
-        </View>
-      </View>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
-    </SafeAreaView>
-  );
-};
+class MainMenuScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            dataSource: ''
+        };
+    }
+    componentDidMount = async () => {
+        fetch(`http://172.20.10.5:5000/api/menus`)
+        .then(response => response.json())
+        .then(responseJson => {
+            this.setState({
+                dataSource: responseJson
+            });
+        })
+    }
+    render() {
+        const Item = ({ menuName }) => (
+            <View style={styles.item}>
+              <Text style={styles.title}>{menuName}</Text>
+            </View>
+        );
+          
+        const renderItem = ({ item }) => (
+            <TouchableOpacity onPress={() => console.log("Here!")}>
+                <Item menuName={item.menuName} />
+            </TouchableOpacity>
+        );
+        return (
+            <SafeAreaView style={styles.container}>
+                <View style={styles.headingContainer}>
+                    <View style={styles.searchContainer}>
+                        <SearchInput placeholder="What are you craving for?" />
+                    </View>
+                </View>
+                <FlatList
+                    data={this.state.dataSource}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.objectId}
+                />
+            </SafeAreaView>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  headingContainer: {
-    height: 100,
-    justifyContent: "center"
-  },  
-  searchContainer: {
-    height: 50,
-  },
-  title: {
-    fontSize: 25,
-  },
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "stretch",
+        marginTop: StatusBar.currentHeight || 0,
+        // backgroundColor: "red"
+    },
+    headingContainer: {
+        height: 100,
+        // backgroundColor: "yellow",
+        justifyContent: "center"
+    },
+    headingText: {
+        fontSize: 35,
+        color: "purple",
+        top: 50,
+        alignSelf: "center"
+    },
+    item: {
+        borderColor: "black",
+        borderWidth: 1,
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    searchContainer: {
+        height: 50,
+    },
+    title: {
+        fontSize: 25,
+    },
 });
 
 export default MainMenuScreen;
