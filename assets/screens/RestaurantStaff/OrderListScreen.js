d9ce248f316c124040fa998dcf3c151cb3e6186a
hariz <hariz1807@gmail.com>
2021-03-29 12:45:23 +0800

    added flatlst

--- assets/screens/RestaurantStaff/OrderListScreen.js
+++ assets/screens/RestaurantStaff/OrderListScreen.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, FlatList } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'Table 1',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Table 2',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Table 3',
+    },
+];
 
 class OrderListScreen extends Component {
     constructor(props) {
@@ -8,12 +24,33 @@ class OrderListScreen extends Component {
 
         };
     }
+    changeLayout = () => {
+        console.log("Here!")
+        // LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        // this.setState({ expanded: fruitName });
+        // this.componentDidMount();
+    }
     render() {
+        const Item = ({ title }) => (
+            <View style={styles.item}>
+              <Text style={styles.title}>{title}</Text>
+            </View>
+        );
+        const renderItem = ({ item }) => (
+            <TouchableOpacity onPress={() => this.changeLayout()}>
+                <Item title={item.title} />
+            </TouchableOpacity>
+        );
         return (
             <SafeAreaView style={styles.container}>
                 <View style={styles.headingContainer}>
                     <Text style={styles.headingText}>Order List Screen</Text>
                 </View>
+                <FlatList
+                    data={DATA}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                />
             </SafeAreaView>
         );
     }
@@ -34,6 +71,16 @@ const styles = StyleSheet.create({
         color: "purple",
         top: 50,
         alignSelf: "center"
+    },
+    item: {
+        borderColor: "black",
+        borderWidth: 1,
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 25,
     },
 });
 
