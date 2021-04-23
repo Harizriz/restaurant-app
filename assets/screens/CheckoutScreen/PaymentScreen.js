78438cd2eb10cf2bad0d92ebb29efb63f472baed
hariz <hariz1807@gmail.com>
2021-04-23 16:42:36 +0800

    added card number functionality

--- assets/screens/CheckoutScreen/PaymentScreen.js
+++ assets/screens/CheckoutScreen/PaymentScreen.js
@@ -1,15 +1,88 @@
 import React, { Component } from 'react';
-import { SafeAreaView, View, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, View, StyleSheet, Text, Alert } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import Icon from "react-native-vector-icons/Octicons"
+import Icon from "react-native-vector-icons/Octicons";
+import { TextInput } from 'react-native-paper';
 import PlusIcon from "react-native-vector-icons/Feather"
 import Button from "react-native-button"
 
 class PaymentScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = { }
+    this.state = { 
+      CardNumberValueHolder: '',
+      ExpDateValueHolder: '',
+      CVVValueHolder: '',
+      isErrorCardNumber: false,
+      isErrorExpDate: false,
+      isErrorCVV: false
+    }
   }
+  validateInput = (cardNumber, expDate, cvvValue) => {
+    const totalPrice = this.props.route.params.cartTotalPrice
+    const tableOrderId = this.props.route.params.tableId
+    const emailData = this.props.route.params.emailData
+
+    // check for inputs in inputtext
+    if(cardNumber == "" || expDate == "" || cvvValue == "") {
+      Alert.alert("Please fill all the fields!", "",
+      { text: "Okay", onPress: () => console.log("Successful") });
+    }
+
+    // validate cardNumber
+    if(cardNumber.trim() == "") {
+      this.setState({
+        CardNumberValueHolder: cardNumber,
+        isErrorCardNumber: true
+      })
+    }
+    else {
+      this.setState({
+        CardNumberValueHolder: cardNumber,
+        isErrorCardNumber: false
+      })
+    }
+
+    // validate expDate
+    if(expDate.trim() == "") {
+      this.setState({
+        ExpDateValueHolder: expDate,
+        isErrorExpDate: true
+      })
+    }
+    else {
+      this.setState({
+        ExpDateValueHolder: expDate,
+        isErrorExpDate: false
+      })
+    }
+
+    // validate cvvValue
+    if(cvvValue.trim() == "") {
+      this.setState({
+        CVVValueHolder: cvvValue,
+        isErrorCVV: true
+      })
+    }
+    else {
+      this.setState({
+        CVVValueHolder: cvvValue,
+        isErrorCVV: false
+      })
+    }
+
+    if (!this.state.CardNumberValueHolder || !this.state.ExpDateValueHolder || !this.state.CVVValueHolder) {
+      console.log("nope")
+    }
+    else {
+      this.props.navigation.navigate("PaymentLoadingScreen",
+        { cartTotalPrice: totalPrice,
+          tableId: tableOrderId,
+          emailData: emailData
+        }
+      )
+    }
+  } 
   render() { 
     const totalPrice = this.props.route.params.cartTotalPrice
     const tableOrderId = this.props.route.params.tableId
@@ -26,8 +99,7 @@ class PaymentScreen extends Component {
         />
       </View>
       <View style={styles.cardsContainer}>
-        <View style={styles.cardContainer}>
-          {/* if this card is chosen show a tick symbol at the right side */}
+        {/* <View style={styles.cardContainer}>
           <TouchableOpacity onPress={() => console.log("Chose")}>
             <Text style={styles.cardInformationText}>
               Visa Ending in 4242
@@ -46,15 +118,59 @@ class PaymentScreen extends Component {
               &nbsp; &nbsp; Add new card
             </Text>
           </TouchableOpacity>
+        </View> */}
+        <View style={styles.mainPromptContainer}>
+          <View style={styles.mainInputContainer}>
+            {/* https://callstack.github.io/react-native-paper/text-input.html */}
+            <TextInput
+                label="  Card Number  "
+                mode="outlined"
+                error={this.state.isErrorCardNumber}
+                textContentType="oneTimeCode"
+                returnKeyType="next"
+                onSubmitEditing={() => { this.dateTextInput.focus(); }}
+                onChangeText={CardNumberValueHolder => this.setState({CardNumberValueHolder})}
+            />
+          </View>
+        </View>
+        <View style={styles.secondPromptContainer}>
+          <View style={styles.secondInputContainer}>
+            <TextInput
+                label="  Expiration Date  "
+                mode="outlined"
+                keyboardType="number-pad"
+                error={this.state.isErrorExpDate}
+                defaultValue="MM/YY"
+                returnKeyType="done"
+                clearTextOnFocus={true}
+                onSubmitEditing={() => { this.CVVTextInput.focus(); }}
+                ref={(input) => { this.dateTextInput = input; }}
+                style={{
+                    top: 20,
+                    width: "45%"
+                }}
+                onChangeText={ExpDateValueHolder => this.setState({ExpDateValueHolder})}
+            />
+            <TextInput
+                label="  CVV/CVN  "
+                mode="outlined"
+                keyboardType="number-pad"
+                error={this.state.isErrorCVV}
+                defaultValue="CVC"
+                clearTextOnFocus={true}
+                returnKeyType="done"
+                ref={(input) => { this.CVVTextInput = input; }}
+                style={{
+                    top: 20,
+                    width: "45%"
+                }}
+                onChangeText={CVVValueHolder => this.setState({CVVValueHolder})}
+            />
+          </View>
         </View>
       </View>
       <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={() => this.props.navigation.navigate("PaymentLoadingScreen",
-          { cartTotalPrice: totalPrice,
-            tableId: tableOrderId,
-            emailData: emailData
-          }
-          )}>
+          <TouchableOpacity onPress={() => this.validateInput(this.state.CardNumberValueHolder, this.state.ExpDateValueHolder, this.state.CVVValueHolder)}>
               <View style={styles.button}>
                   <Text style={styles.buttonText}>Place Order</Text>
               </View>
@@ -115,7 +231,27 @@ const styles = StyleSheet.create({
       width: "75%",
       alignSelf: "center",
       marginBottom: 20,
-    }
+    },
+    mainPromptContainer: {
+      height: 65,
+      alignItems: "center",
+      // backgroundColor: "blue"
+      top: 20
+    },
+    mainInputContainer: {
+      width: "75%",
+    },
+    secondPromptContainer: {
+      height: 70,
+      alignItems: "center",
+      // backgroundColor: "red",
+      top: 20
+    },
+    secondInputContainer: {
+      width: "75%",
+      flexDirection: "row",
+      justifyContent: "space-between"
+    },
   });
  
 export default PaymentScreen;
