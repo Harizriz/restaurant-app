8a55478f1b5c13079792ed13ae98826b1ba8a5cb
hariz <hariz1807@gmail.com>
2021-04-23 15:10:58 +0800

    edit frontend

--- assets/screens/LoginScreen/ResetPasswordScreen.js
+++ assets/screens/LoginScreen/ResetPasswordScreen.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, View, Text } from "react-native";
-import PromptTextInput from "../../components/InputText";
-import AlertButton from "../../components/AlertButton";
+import React, { Component } from "react";
+import { SafeAreaView, View, StyleSheet, Text, Alert } from "react-native";
+import Button from "react-native-button";
+import { TextInput } from 'react-native-paper'
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 class ResetPasswordScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = { 
+      EmailValueHolder: '',
+      isError: false
+    };
   }
   render() {
     return (
@@ -15,13 +19,38 @@ class ResetPasswordScreen extends Component {
           <Text style={styles.headingText}>Reset Password</Text>
         </View>
         <View style={styles.promptContainer}>
-          <PromptTextInput placeholder="Email" />
+          <View style={styles.textInputContainer}>
+            <TextInput
+              label="  Email  "
+              mode="outlined"
+              error={this.state.isError}
+              returnKeyType="done"
+              style={{
+                  top: 20,
+                  height: 50
+              }}
+              onChangeText={EmailValueHolder => this.setState({EmailValueHolder})}
+            />      
+          </View>
         </View>
         <View style={styles.submitContainer}>
-          <AlertButton
-            buttonText="Send Link"
-            text="Link has been successfully sent to your Email!"
-          />
+          <Button
+            style={{
+              padding: 16,
+              width: 250,
+              borderRadius: 24,
+              alignItems: "center",
+              backgroundColor: "purple",
+              color: "white",
+              overflow: "hidden",
+              top: 30,
+            }}
+            onPress={() => {
+              
+            }}
+          >
+            Send
+          </Button>
         </View>
       </SafeAreaView>
     );
@@ -39,7 +68,8 @@ const styles = StyleSheet.create({
     // backgroundColor: "yellow",
   },
   promptContainer: {
-    height: 100,
+    height: 90,
+    alignItems: "center",
     // backgroundColor: "red",
   },
   submitContainer: {
@@ -47,12 +77,51 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     // backgroundColor: "green",
   },
-  headingText: {
-    fontSize: 40,
-    color: "purple",
-    left: 50,
-    top: 50,
+  textInputContainer: {
+    width: "75%",
+    flex: 1,
+    // flexDirection: "row",
+    // justifyContent: "center",
+    // alignItems: "center",
+    // backgroundColor: "yellow",
+    // borderWidth: 1,
   },
+  headingText: {
+    fontSize: 35,
+    color: "purple",
+    top: 50,
+    alignSelf: "center"
+  },
+  link: {
+    justifyContent: "flex-end",
+    alignSelf: "flex-end",
+    right: 50,
+    color: "blue",
+    // backgroundColor: "red",
+    bottom: 20
+  },
+  loginLink: {
+    alignSelf: "center",
+    color: "blue",
+    paddingTop: 30,
+  },
+  or: {
+    height: 50,
+    alignSelf: "center",
+    // backgroundColor: "orange",
+    paddingTop: 10,
+  },
+  bottomContainer: {
+    height: 200,
+    alignSelf: "center",
+    // backgroundColor: "pink",
+  },
+  icon: {
+    left: 125,
+    bottom: 27,
+    marginHorizontal: 147,
+    // backgroundColor: "yellow",
+  }
 });
 
 export default ResetPasswordScreen;
